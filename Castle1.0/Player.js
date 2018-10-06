8370a8d795c95d2aeab7b822925325c397ab92a1
aidandom <39746226+aidandom@users.noreply.github.com>
2018-10-06 12:53:26 -0500

    Add files via upload

---
+++ Castle1.0/Player.js
@@ -0,0 +1,103 @@
+var cursor,sword;
+Hero=function(game,x,y){
+Phaser.Sprite.call(this,game,x,y,'hero');
+this.scale.setTo(0.3,0.3);
+this.anchor.setTo(0.5,0.5);
+this.game.physics.enable(this);
+this.body.collideWorldBounds=true;
+this.animations.add('walk',[0,1,2,3,4,5,6,7,8,9,10,11,12]);
+this.animations.add('walkUp',[13,14,15]);
+this.animations.add('walkDown',[16,17,18]);
+this.animations.add('attack',[19,20,21,22,23,24]);
+this.animations.add('attackUp',[25,26,27,28]);
+this.animations.add('attackDown',[29,30,31,32,33,34]);    
+this.life=100;
+this.aKey=game.input.keyboard.addKey(Phaser.Keyboard.A);
+this.attack=false;
+
+cursor=this.game.input.keyboard.createCursorKeys();
+
+
+game.add.existing(this);
+}
+Hero.prototype=Object.create(Phaser.Sprite.prototype);
+Hero.prototype.constructor=Hero;
+
+
+
+Hero.prototype.update=function(){
+
+this.playerMove();
+if(this.attack==true){
+console.log(this.attack);}
+game.physics.arcade.collide(this,rock, function(){console.log('wall hit');});
+this.playerAttack();
+
+}
+
+Hero.prototype.playerMove = function(){
+
+  if(cursor.up.isDown){//needs velocity to collision 
+     this.body.velocity.y=-vel;
+     this.animations.play('walkUp',12,false);
+    }
+    else if(cursor.down.isDown){
+     this.body.velocity.y=vel;
+     this.animations.play('walkDown',12,false);
+
+
+    }
+    else{
+     this.body.velocity.y=0
+
+    }
+
+    if(cursor.left.isDown){
+     this.animations.play('walk',20,false);
+     this.body.velocity.x=-vel;
+     this.scale.setTo(-0.3,0.3);
+     this.playerAttack();
+
+
+    }
+    else if(cursor.right.isDown){
+     this.animations.play('walk',20,false);
+     this.body.velocity.x=vel;
+     this.scale.setTo(0.3,0.3);
+
+
+    }
+
+    else{
+     this.body.velocity.x=0;
+     this.animations.stop('walk',1);
+     //couldn't find a way to set the frame to 0
+
+    }
+
+
+}
+Hero.prototype.playerAttack=function(){
+  if(this.aKey.isDown&&this.attack==false){
+     this.animations.play('attack',70,false);
+      this.attack=true;
+      this.playerSword();
+      game.time.events.add(Phaser.Timer.SECOND*.3,function(){this.attack=false;},this)
+        game.time.events.add(Phaser.Timer.SECOND*.2,function(){
+          this.animations.stop('attack',1);
+          sword.kill();
+
+        },this)
+    }
+
+}
+
+Hero.prototype.playerSword=function(){
+  sword=this.addChild(this.game.add.sprite(140,0,'slash'));
+  sword.game.physics.enable(sword);
+  sword.anchor.setTo(0.5,0.5);
+  sword.animations.add('slashAttack',[0,1,2,3,4]);
+  sword.animations.play('slashAttack',70,false);
+
+
+}
---
+++ Castle1.0/state1.js
@@ -0,0 +1,225 @@
+var sound,deathsound;
+var demo={},centerX=1500/2,centerY=1000/2,adam,speed=4;
+var vel=400,rock,grass,enemy,enemyGroup,scoreText,lifeText;
+var score=0,life=50,lifeString='',scoreString='',livingEnemies=[];
+var hero,boss;
+demo.state1 = function(){};
+demo.state1.prototype ={
+preload: function(){
+
+  game.load.tilemap('dungeon','assets/tilemaps/dungeon.json',null,Phaser.Tilemap.TILED_JSON);
+  game.load.image('grass','assets/background/back-ground.png');
+  game.load.spritesheet('hero','assets/spritesheets/demoCastleHero.png',320,320);
+  game.load.spritesheet('boss','assets/spritesheets/demoBossSheet.png',320,320);
+  game.load.spritesheet('slash','assets/spritesheets/heroSwordSlash.png',90,170);
+  game.load.audio('end','assets/sounds/Dark_Dungeon_AMBIENT_LOOP.mp3');
+  game.load.image('enemy','assets/spritesheets/enemy.png',240,302);
+  game.load.image('tile_1','assets/tilemaps/tile_1.png');
+  game.load.image('tile_2','assets/tilemaps/tile_2.png');
+  game.load.image('tile_3','assets/tilemaps/tile_3.png');
+  game.load.audio('death_sound','assets/sounds/ANIMAL_Bird_Crow_01_mono.mp3');
+
+
+},
+create: function(){
+
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+
+  game.stage.backgroundColor='#DDDDDD';
+  console.log('state0');
+  addChangeStateEventListener();
+
+////////map//////
+  var map= game.add.tilemap('dungeon');
+  map.addTilesetImage('tile_1');
+  map.addTilesetImage('tile_2');
+  map.addTilesetImage('grass');
+  map.addTilesetImage('tile_3');
+
+  grass=map.createLayer('tile_1');
+  rock=map.createLayer('tile_3');
+
+
+
+  map.setCollisionBetween(12,14,true,'tile_3');
+  map.setCollisionBetween(1,true,'tile_1');
+/////////////
+
+
+hero=new Hero(game,750,900);
+boss=new Boss(game,50,400);
+
+
+
+////sound///
+
+sound=game.add.audio('end');
+sound.addMarker('end',0,240);
+sound.play('end');
+deathsound=game.add.audio('death_sound');
+deathsound.addMarker('death_sound',0,2);
+/////////////
+
+
+
+/////enemy////
+
+// enemy=game.add.sprite(100,200,'enemy');
+// enemy.scale.setTo(0.5,0.5);
+// game.physics.enable(enemy);
+
+ enemyGroup=game.add.group();
+ enemyGroup.enableBody=true;
+ enemyGroup.physicsBodyType=Phaser.Physics.ARCADE;
+
+for(var i=0;i<5; i++){
+
+  enemyGroup.create(500,150*i+100,'enemy');
+}
+enemyGroup.setAll('anchor.y',0.5);
+enemyGroup.setAll('anchor.x',0.5);
+enemyGroup.setAll('scale.y',0.15);
+enemyGroup.setAll('scale.x',0.15);
+
+
+//game.add.tween(enemy).to({x:'+400'}, 2000,'Quad.easeOut',true,100,false,true).loop(true);
+game.add.tween(enemyGroup).to({x:'+400'}, 2000,'Quad.easeOut',true,100,false,true).loop(true);
+
+
+////////
+
+
+  game.world.setBounds(0,0,2813,1000);
+
+  game.scale.scaleMode=Phaser.ScaleManager.SHOW_ALL;
+
+
+   /////
+
+
+//////
+   ////score////
+   scoreString=('Kills: ');
+   scoreText = game.add.text(10, 10, scoreString + score, { font: '100px Arial', fill: '#fff' });
+   lifeString=('Life: ');
+   lifeText = game.add.text(10, 150, lifeString + life, { font: '100px Arial', fill: '#fff' });
+
+   /////
+
+},
+update: function(){
+
+  game.physics.arcade.collide(hero,rock, function(){console.log('wall hit');});
+  game.physics.arcade.collide(boss,rock, function(){console.log('wall hit');});
+  game.physics.arcade.collide(boss,hero, function(){console.log('wall hit');});
+  game.physics.arcade.overlap(sword,boss,this.hitEnemy);
+  //game.physics.arcade.collide(adam,grass, function(){console.log('rock hit');});
+  game.physics.arcade.overlap(sword,enemyGroup,this.hitEnemyGroup);
+  game.physics.arcade.collide(hero,enemyGroup,this.hitbyEnemyGroup);
+  livingen();
+  enemyTowardsPlayer(boss,hero);
+  console.log(boss.life);
+
+
+},
+
+render: function(){
+///bug testing bounderis
+  //game.debug.geom(sword.getBounds());
+  //game.debug.geom(enemyGroup.getBounds());
+}
+,
+hitEnemy: function(){
+game.physics.arcade.collide (sword,boss,function(){
+  if(boss.life==-20){
+    boss.kill();
+  }
+else{boss.life-=1;}
+life-=1
+lifeText.text=lifeString+life;score+=1;})
+
+
+scoreText.text=scoreString+score;
+ deathsound.play('death_sound');
+},
+
+hitEnemyGroup: function(b,e){
+//console.log('hitgroup');
+
+e.kill();
+score+=1;
+scoreText.text=scoreString+score;
+deathsound.play('death_sound');
+},
+
+hitbyEnemyGroup: function(b,e){
+console.log('hitbygroup');
+
+life-=1;
+lifeText.text=lifeString+life;
+scoreText.text=scoreString+score;
+deathsound.play('death_sound');
+}
+};
+
+
+function changeState(i,stateNum){
+  console.log('state'+stateNum);
+  game.state.start('state'+stateNum);
+
+}
+function addKeyCallback(key,fn,args){
+
+  game.input.keyboard.addKey(key).onDown.add(fn,null,null,args);
+
+}
+
+function addChangeStateEventListener(){
+
+  addKeyCallback(Phaser.Keyboard.ZERO,changeState,0);
+  addKeyCallback(Phaser.Keyboard.ONE,changeState,1);
+
+
+}
+function livingen(){
+  //console.log('working?');
+  livingEnemies.length=0;
+  enemyGroup.forEachAlive(function(enemy){
+   livingEnemies.push(enemy);
+ })
+
+}
+function enemyTowardsPlayer(e,a){
+  targetAngle=game.math.angleBetween(e.x,e.y,a.x,a.y);
+  delta=targetAngle*(180/Math.PI)
+  if(e.roation!=targetAngle){
+
+
+    if(delta>-90&&delta<90){
+      //console.log(delta);
+      e.body.velocity.x = 200;
+      if(delta<140&&delta>70){
+        //onsole.log(delta);
+        e.body.velocity.y = 200;
+      }
+      else if(delta<-70&&delta>-140){
+        //console.log(delta);
+        e.body.velocity.y = -200;
+
+      }
+      else if(delta<20&&delta>-20){
+        console.log(delta);
+        e.body.velocity.x = 200;
+
+      }
+
+    }
+
+    else{
+      e.body.velocity.x = -200;
+
+    }
+
+  }
+
+}
