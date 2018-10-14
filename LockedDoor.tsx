f3abe072107e37cfc1f08366aee4d713fb39a641
reethikathakkalapally <43014389+reethikathakkalapally@users.noreply.github.com>
2018-10-14 15:18:08 -0500

    Add files via upload

---
+++ LockedDoor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.1" tiledversion="2018.09.12" name="LockedDoor" tilewidth="32" tileheight="32" tilecount="25" columns="5">
+ <image source="../Downloads/phaser_tutorial_02/assets/LockedDoor.png" width="190" height="190"/>
+</tileset>
