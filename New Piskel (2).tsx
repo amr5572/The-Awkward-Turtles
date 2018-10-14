6a300cd2d8b7ffdbb8e44070dbff8f1a4f830de9
reethikathakkalapally <43014389+reethikathakkalapally@users.noreply.github.com>
2018-10-14 15:12:17 -0500

    Add files via upload

---
+++ New Piskel (2).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.1" tiledversion="2018.09.12" name="New Piskel (2)" tilewidth="32" tileheight="32" tilecount="961" columns="31">
+ <image source="../Downloads/New Piskel (2).png" width="1000" height="1000"/>
+</tileset>
---
+++ New Piskel (7).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.1" tiledversion="2018.09.12" name="New Piskel (7)" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="../Downloads/New Piskel (7).png" width="32" height="32"/>
+</tileset>
