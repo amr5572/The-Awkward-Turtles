d153557a4a5b774f356d69073ea2cbd167bb2a33
reethikathakkalapally <43014389+reethikathakkalapally@users.noreply.github.com>
2018-11-06 21:57:11 -0600

    Delete otherpot.tsx

--- otherpot.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.1" tiledversion="2018.09.12" name="otherpot" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../Downloads/otherpot.png" width="32" height="32"/>
-</tileset>
